b0154b6b6fd18ff390d8e555909718410a3f588c
vphuc81 <vinhphuc_81@yahoo.com>
2015-06-22 20:44:13 -0700

    sporstdevil

---
+++ plugin.video.SportsDevil/lib/dialogs/dialogInfo.py
@@ -0,0 +1,12 @@
+# -*- coding: latin-1 -*-
+
+import xbmcgui
+
+class DialogInfo:
+
+    def __init__(self):
+        self.dlg = xbmcgui.Dialog()
+        self.head = 'SportsDevil Info'
+
+    def show(self, message):
+        self.dlg.ok(self.head, message)
