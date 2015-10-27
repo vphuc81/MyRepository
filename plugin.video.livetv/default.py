cc199abe2a98eff48c15024a902367059172a6cf
vphuc81 <vinhphuc_81@yahoo.com>
2015-10-26 19:28:56 -0700

    update

--- plugin.video.livetv/default.py
+++ plugin.video.livetv/default.py
@@ -44,7 +44,7 @@ def TVChannel(url):
                ("tomovieshd2" in link) or ("tocartoons8" in link) or ("tokiddiecartoons" in link) or ("tonavix" in link) or \
                ("toxmovies8" in link) or ("togenesis" in link) or ("tophoenix" in link) or ("toanhtrang" in link) or \
                ("tophim14" in link) or ("tofootball" in link) or ("toxemphimso" in link) or ("toenter" in link) or \
-               ("toitv" in link) or ("tofilmon1" in link) or ("tofilmon2" in link): 
+               ("toitv" in link) or ("tofilmon1" in link) or ("tofilmon2" in link) or ("tozeus" in link): 
                 link = re.compile('<link>(.+?)</link>').findall(item)[0]            
             add_Link(title, link, thumb)
         xbmc.executebuiltin('Container.SetViewMode(52)')		
@@ -372,6 +372,10 @@ def add_Link(name,url,iconimage):
         u = 'plugin://plugin.video.filmontv'  
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
+    if 'tozeus' in url:
+        u = 'plugin://plugin.video.zeus'  
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
     ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)  
 
 def addLink(name,url,mode,iconimage):
@@ -431,7 +435,7 @@ def parameters_string_to_dict(parameters):
 
 DecryptData = base64.b64decode	
 homeurl = 'aHR0cHM6Ly9nb29nbGVkcml2ZS5jb20vaG9zdC8wQjd6a2tRd281cHI1ZmpOVWNIY3dNRWt5UzBn\
-eFVHUnZRbE16YUdweFNEUXRaR050VVZaclN5MWZRemhEU2pKMU5FaG1ibU0vdjExLnhtbA=='
+eFVHUnZRbE16YUdweFNEUXRaR050VVZaclN5MWZRemhEU2pKMU5FaG1ibU0vdjEyLnhtbA=='
 params=parameters_string_to_dict(sys.argv[2])
 mode=params.get('mode')
 url=params.get('url')
