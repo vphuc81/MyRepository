599be3f05869aed33d4dc1559ea8ba881baa7141
vphuc81 <vinhphuc_81@yahoo.com>
2015-11-06 11:41:36 -0800

    update

--- plugin.video.livetv/default.py
+++ plugin.video.livetv/default.py
@@ -435,7 +435,7 @@ def parameters_string_to_dict(parameters):
 
 DecryptData = base64.b64decode	
 homeurl = 'aHR0cHM6Ly9nb29nbGVkcml2ZS5jb20vaG9zdC8wQjd6a2tRd281cHI1ZmpOVWNIY3dNRWt5UzBn\
-eFVHUnZRbE16YUdweFNEUXRaR050VVZaclN5MWZRemhEU2pKMU5FaG1ibU0vdjEyLnhtbA=='
+eFVHUnZRbE16YUdweFNEUXRaR050VVZaclN5MWZRemhEU2pKMU5FaG1ibU0vdjEzLnhtbA=='
 params=parameters_string_to_dict(sys.argv[2])
 mode=params.get('mode')
 url=params.get('url')
