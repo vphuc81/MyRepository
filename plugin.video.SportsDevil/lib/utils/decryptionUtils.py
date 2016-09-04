7f384768233a29566cc9a72e8c7f4fc862bfb6e4
vphuc81 <vinhphuc_81@yahoo.com>
2016-09-04 12:22:21 -0700

    update

--- plugin.video.SportsDevil/lib/utils/decryptionUtils.py
+++ plugin.video.SportsDevil/lib/utils/decryptionUtils.py
@@ -4,6 +4,9 @@ import urllib
 import re
 from regexUtils import parseTextToGroups
 from javascriptUtils import JsFunctions, JsUnpacker, JsUnpackerV2, JsUnpacker95High, JsUnwiser, JsUnIonCube, JsUnFunc, JsUnPP, JsUnPush
+from hivelogic import hivelogic
+try: import json
+except ImportError: import simplejson as json
 
 def encryptDES_ECB(data, key):
     data = data.encode()
@@ -12,17 +15,24 @@ def encryptDES_ECB(data, key):
     assert k.decrypt(d, padmode=pyDes.PAD_PKCS5) == data
     return d
 
+def decryptDES_ECB(data, key):
+    data = data.decode('base-64')
+    k = pyDes.des(key, pyDes.ECB, IV=None, pad=None, padmode=pyDes.PAD_PKCS5)
+    return k.decrypt(data, padmode=pyDes.PAD_PKCS5)
+
 def gAesDec(data, key):
     from mycrypt import decrypt
     return decrypt(key,data)
 
 def cjsAesDec(data, key):
-    try: import json
-    except ImportError: import simplejson as json
     from mycrypt import decrypt
     enc_data = json.loads(data.decode('base-64'))
     ciphertext = 'Salted__' + enc_data['s'].decode('hex') + enc_data['ct'].decode('base-64')
     return json.loads(decrypt(key,ciphertext.encode('base-64')))
+
+def drenchDec(data, key):
+    from drench import blowfish
+    return blowfish(key).decrypt(data)
 
 def wdecode(data):
     from itertools import chain
@@ -44,6 +54,34 @@ def wdecode(data):
         if ring >= len(codec):
             ring = 0
     return ''.join(res)
+
+def onetv(playpath):
+    import random,time,md5
+    from base64 import b64encode
+    user_agent = 'Mozilla%2F5.0%20%28Linux%3B%20Android%205.1.1%3B%20Nexus%205%20Build%2FLMY48B%3B%20wv%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Version%2F4.0%20Chrome%2F43.0.2357.65%20Mobile%20Safari%2F537.36'
+    token = "65rSw"+"UzRad"
+    servers = [ '176.31.231.58',
+                '167.114.159.23',
+                '167.114.159.80',
+                '178.33.231.148',
+                '5.196.86.78',
+                '176.31.237.61',
+                '192.99.149.33',
+                '5.196.85.47',
+                '37.59.17.46',
+                '46.105.119.117',
+                '51.254.43.148',
+                '192.99.19.181',
+                '5.196.85.58',
+                '192.99.19.176']
+    time_stamp = str(int(time.time()) + 14400)
+    to_hash = "{0}{1}/hls/{2}".format(token,time_stamp,playpath)
+    out_hash = b64encode(md5.new(to_hash).digest()).replace("+", "-").replace("/", "_").replace("=", "")
+    server = random.choice(servers)
+    
+    url = "hls://http://{0}/p2p/{1}?st={2}&e={3}".format(server,playpath,out_hash,time_stamp)
+    return '{url}|User-Agent={user_agent}&referer={referer}'.format(url=url,user_agent=user_agent,referer='6d6f6264726f2e6d65'.decode('hex'))
+    
 
 def encryptJimey(data):
     result = encryptDES_ECB(data,"PASSWORD").encode('base64').replace('/','').strip()
@@ -82,6 +120,7 @@ def doDemystify(data):
     jsUP = JsUnPP()
     jsU95 = JsUnpacker95High()
     JsPush = JsUnPush()
+    JsHive = hivelogic()
 
     # replace NUL
     #data = data.replace('\0','')
@@ -117,10 +156,10 @@ def doDemystify(data):
                     res = res + chr(ord(i) ^ 123)
             data = data.replace(g, res)
             
-    r = re.compile('(eval\(decodeURIComponent\(atob\([\'"][^\'"]+[\'"]\)\)\);)')
+    r = re.compile('((?:eval\(decodeURIComponent\(|window\.)atob\([\'"][^\'"]+[\'"]\)+)')
     while r.findall(data):
         for g in r.findall(data):
-            r2 = re.compile('eval\(decodeURIComponent\(atob\([\'"]([^\'"]+)[\'"]\)\)\);')
+            r2 = re.compile('(?:eval\(decodeURIComponent\(|window\.)atob\([\'"]([^\'"]+)[\'"]\)+')
             for base64_data in r2.findall(g):
                 data = data.replace(g, urllib.unquote(base64_data.decode('base-64')))
                 
@@ -147,12 +186,27 @@ def doDemystify(data):
         except:
             pass
 
+    if '"result2":"'in data:
+        r = re.compile(r""":("(?!http)[^\.]+M3U4")""")
+        gs = r.findall(data)
+        if gs:
+            for g in gs:
+                data = data.replace(g,json.dumps(decryptDES_ECB(json.loads(g).decode('base64')[:-5], '5333637233742600'.decode('hex'))))
     # n98c4d2c
     if 'function n98c4d2c(' in data:
         gs = parseTextToGroups(data, ".*n98c4d2c\(''\).*?'(%[^']+)'.*")
         if gs != None and gs != []:
             data = data.replace(gs[0], jsF.n98c4d2c(gs[0]))
-
+            
+    if 'var enkripsi' in data:
+        r = re.compile(r"""enkripsi="([^"]+)""")
+        gs = r.findall(data)
+        if gs:
+            for g in gs:
+                s=''
+                for i in g:
+                    s+= chr(ord(i)^2)
+                data = data.replace("""enkripsi=\""""+g, urllib.unquote(s))
     # o61a2a8f
     if 'function o61a2a8f(' in data:
         gs = parseTextToGroups(data, ".*o61a2a8f\(''\).*?'(%[^']+)'.*")
@@ -192,9 +246,9 @@ def doDemystify(data):
                 data = data.replace(g, jsF.pbbfa0(g))
     
     if 'eval(function(' in data:
-        data = re.sub(r"""function\(\w\w\w,\w\w\w,\w\w\w,\w\w\w\)""",'function(p,a,c,k)',data)
-        data = re.sub(r"""\(\w\w\w\+0\)%\w\w\w""",'e%a',data)
-        data = re.sub(r"""RegExp\(\w\w\w\(\w\w\w\)""",'RegExp(e(c)',data)
+        data = re.sub(r"""function\(\w\w\w\w,\w\w\w\w,\w\w\w\w,\w\w\w\w""",'function(p,a,c,k)',data.replace('#','|'))
+        data = re.sub(r"""\(\w\w\w\w\+0\)%\w\w\w\w""",'e%a',data)
+        data = re.sub(r"""RegExp\(\w\w\w\w\(\w\w\w\w\)""",'RegExp(e(c)',data)
         
     if """.replace(""" in data:
         r = re.compile(r""".replace\(["']([^"']+)["'],\s*["']([^"']*)["']\)""")
@@ -218,7 +272,7 @@ def doDemystify(data):
         if gs:
             for g in gs:
                 data = data.replace(g, destreamer(g))
-
+                
     # JS P,A,C,K,E,D
     if jsU95.containsPacked(data):
         data = jsU95.unpackAll(data)
@@ -253,6 +307,9 @@ def doDemystify(data):
         
     if JsPush.containUnPush(data):
         data = JsPush.UnPush(data)
+
+    if JsHive.contains_hivelogic(data):
+        data = JsHive.unpack_hivelogic(data)
 
     # unescape again
     if escape_again:
