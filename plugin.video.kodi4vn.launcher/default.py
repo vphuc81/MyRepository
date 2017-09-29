bfc5b4127633a5fdf80de379ba56ae220caaf8e5
vphuc81 <vinhphuc_81@yahoo.com>
2017-09-29 10:13:03 -0700

    update

--- plugin.video.kodi4vn.launcher/default.py
+++ plugin.video.kodi4vn.launcher/default.py
@@ -213,42 +213,42 @@ def dir ( url , title = "" ) :
      if 21 - 21: o0OIiiIII111iI * I11i11Ii % O0OOo * oo000i1iIi11iIIi1
      if 16 - 16: ooO0OO000o - Ooooo * I11i11Ii + IiIi1Iii1I1
      if 50 - 50: Iii1IIIiiI - i1iIIIiI1I * ooO00oOoo / Ooooo + iiI1i1
-   O0O0O = ( "" if O0 . get_setting ( "email" ) == "" else ( "Chào %s. " % O0 . get_setting ( "email" ) ) )
-   oO0Oo = [ {
- "label" : "[COLOR yellow][B]%sVào đây để Login/Relogin[/B][/COLOR]" % O0O0O ,
- "path" : iI11I1II1I1I + "/login" ,
- "thumbnail" : "https://cdn3.iconfinder.com/data/icons/gray-toolbar-2/512/login_user_profile_account-512.png"
- } ]
-   oO00oOo = oO00oOo + oO0Oo
+     if 88 - 88: I1Ii / Ooooo + IiIi1Iii1I1 - Iii1IIIiiI / i1iIIIiI1I - Ii11111i
+     if 15 - 15: ooO00oOoo + Ii11111i - O0O / i1iII1I1i1i1
+     if 58 - 58: ii1I % i1iIIII
+     if 71 - 71: i1iII1I1i1i1 + i1iIIIiI1I % ii1I + ooO00oOoo - O0O0O0O00OooO
+     if 88 - 88: Ii11111i - o0OIiiIII111iI % i1iII1I1i1i1
+     if 16 - 16: iI * O0OOo % O0O0O0O00OooO
+     if 86 - 86: iI + I1Ii % ii1I * O0OOo . i1iIIIiI1I * i1iIIII
  except :
   oo000 = xbmc . translatePath ( xbmcaddon . Addon ( ) . getAddonInfo ( 'path' ) ) . decode ( "utf-8" )
-  oOOoo0Oo = xbmc . translatePath ( os . path . join ( oo000 , "error_icon.jpg" ) )
-  o00OO00OoO = xbmc . translatePath ( os . path . join ( oo000 , "error_bg.jpg" ) )
-  OOOO0OOoO0O0 = xbmc . translatePath ( os . path . join ( oo000 , "error_fullscreen.jpg" ) )
-  if 65 - 65: O0O0O0O00OooO * iI + I1Ii % ii1I * O0OOo . Ooooo
-  OoO0O00 = [ {
+  i1I11i1iI = xbmc . translatePath ( os . path . join ( oo000 , "error_icon.jpg" ) )
+  I1ii1Ii1 = xbmc . translatePath ( os . path . join ( oo000 , "error_bg.jpg" ) )
+  iii11 = xbmc . translatePath ( os . path . join ( oo000 , "error_fullscreen.jpg" ) )
+  if 68 - 68: o0OIiiIII111iI
+  IIi1iIIiI = [ {
  "label" : "Connection Error! OK Here for more details" ,
- "path" : "%s/showimage/%s" % ( iI11I1II1I1I , urllib . quote_plus ( OOOO0OOoO0O0 ) ) ,
- "thumbnail" : oOOoo0Oo ,
- "properties" : { 'fanart_image' : o00OO00OoO }
+ "path" : "%s/showimage/%s" % ( iI11I1II1I1I , urllib . quote_plus ( iii11 ) ) ,
+ "thumbnail" : i1I11i1iI ,
+ "properties" : { 'fanart_image' : I1ii1Ii1 }
  } ]
-  oO00oOo += OoO0O00
+  oO00oOo += IIi1iIIiI
  return O0 . finish ( oO00oOo )
- if 5 - 5: Oo / iiI1i1 . I1Ii - ooO0OO000o / O0O0O0O00OooO
+ if 58 - 58: i1iIIIiI1I / Iii1IIIiiI - i1iII1I1i1i1 - ii1I % Ii11111i - Ooooo
 @ O0 . route ( '/ytp/<pid>' , name = 'ytp_firstpage' )
 @ O0 . route ( '/ytp/<pid>/<page>' )
-def ooOooo000oOO ( pid , page = "" ) :
- Oo0oOOo = ""
+def IIII11I1I ( pid , page = "" ) :
+ OOO0o = ""
  if " - " in pid :
-  Oo0OoO00oOO0o = pid . split ( " - " )
-  Oo0oOOo = " - " . join ( Oo0OoO00oOO0o [ 1 : ] )
-  pid = Oo0OoO00oOO0o [ 0 ]
-  if 80 - 80: O0OOo + i1iII1I1i1i1 - i1iII1I1i1i1 % IiIi1Iii1I1
- i1IIi11111i ( "Browse YT Videos by Playlist [%s]" % Oo0oOOo , "/ytp/%s/%s/%s" % ( Oo0oOOo , pid , page ) )
+  IiI1 = pid . split ( " - " )
+  OOO0o = " - " . join ( IiI1 [ 1 : ] )
+  pid = IiI1 [ 0 ]
+  if 54 - 54: Iii1IIIiiI % Ii11111i % i1iIIII % I11i11Ii + I11i11Ii * i1iIIIiI1I
+ i1IIi11111i ( "Browse YT Videos by Playlist [%s]" % OOO0o , "/ytp/%s/%s/%s" % ( OOO0o , pid , page ) )
  oO00oOo = IiII ( "%s/ytp/%s/%s" % ( oooo , pid , page ) )
  for iII11i in oO00oOo :
   iII11i [ "path" ] = iI11I1II1I1I + iII11i [ "path" ]
-  OoOO0oo0o ( iII11i )
+  O00O0oOO00O00 ( iII11i )
  if O0 . get_setting ( 'thumbview' , bool ) :
   if xbmc . getSkinDir ( ) in ( 'skin.confluence' , 'skin.eminence' ) :
    return O0 . finish ( oO00oOo , view_mode = 500 )
@@ -258,22 +258,22 @@ def ooOooo000oOO ( pid , page = "" ) :
    return O0 . finish ( oO00oOo )
  else :
   return O0 . finish ( oO00oOo )
-  if 14 - 14: iiI1i1 * IiIi1Iii1I1 * IiIi1Iii1I1 / Ii11111i
+  if 11 - 11: O0O0O0O00OooO . ooO00oOoo
 @ O0 . route ( '/ytu/<uid>' )
-def Oo0o00 ( uid ) :
- O0O0oOO00O00o = requests . get ( "%s/ytu/%s" % ( oooo , uid ) ) . text
- iI1ii11iIi1i ( O0O0oOO00O00o + " - " + uid , "" )
- if 50 - 50: O0O0O0O00OooO
+def o0oo0oOo ( uid ) :
+ o000O0o = requests . get ( "%s/ytu/%s" % ( oooo , uid ) ) . text
+ iI1iII1 ( o000O0o + " - " + uid , "" )
+ if 86 - 86: i1iII1I1i1i1
 @ O0 . route ( '/ytc/<cid>' , name = 'ytc_firstpage' )
 @ O0 . route ( '/ytc/<cid>/<page>' )
-def iI1ii11iIi1i ( cid , page = "" ) :
- Oo0oOOo = ""
+def iI1iII1 ( cid , page = "" ) :
+ OOO0o = ""
  if " - " in cid :
-  i11I1iIiII = cid . split ( " - " )
-  Oo0oOOo = " - " . join ( i11I1iIiII [ 1 : ] )
-  cid = i11I1iIiII [ 0 ]
-  if 96 - 96: Oo
- i1IIi11111i ( "Browse YT Videos by Channel [%s]" % Oo0oOOo , "/ytc/%s/%s/%s" % ( Oo0oOOo , cid , page ) )
+  OOoo0O = cid . split ( " - " )
+  OOO0o = " - " . join ( OOoo0O [ 1 : ] )
+  cid = OOoo0O [ 0 ]
+  if 67 - 67: ii1I - oo000i1iIi11iIIi1 % ooO00oOoo . ooO0OO000o
+ i1IIi11111i ( "Browse YT Videos by Channel [%s]" % OOO0o , "/ytc/%s/%s/%s" % ( OOO0o , cid , page ) )
  oO00oOo = [ {
  "label" : "[B]Playlist[/B]" ,
  "path" : "%s/ytcp/%s/%s" % ( iI11I1II1I1I , cid . split ( "@" ) [ 0 ] , "" ) ,
@@ -285,7 +285,7 @@ def iI1ii11iIi1i ( cid , page = "" ) :
   iiii11I = IiII ( "%s/ytp/%s/%s" % ( oooo , cid . split ( "@" ) [ 1 ] , page ) )
   for iII11i in iiii11I :
    iII11i [ "path" ] = iI11I1II1I1I + iII11i [ "path" ]
-   OoOO0oo0o ( iII11i )
+   O00O0oOO00O00 ( iII11i )
   oO00oOo += iiii11I
  if O0 . get_setting ( 'thumbview' , bool ) :
   if xbmc . getSkinDir ( ) in ( 'skin.confluence' , 'skin.eminence' ) :
@@ -296,21 +296,21 @@ def iI1ii11iIi1i ( cid , page = "" ) :
    return O0 . finish ( oO00oOo )
  else :
   return O0 . finish ( oO00oOo )
-  if 45 - 45: ooO0OO000o * iiI1i1 % Oo * O0O + IiIi1Iii1I1 . Ii11111i
+  if 77 - 77: O0O0O0O00OooO / iI
 @ O0 . route ( '/ytcp/<cid>' , name = 'ytcp_firstpage' )
 @ O0 . route ( '/ytcp/<cid>/<page>' )
-def Oo0ooOo0o ( cid , page = "" ) :
- Oo0oOOo = ""
+def I1iiIii ( cid , page = "" ) :
+ OOO0o = ""
  if " - " in cid :
-  i11I1iIiII = cid . split ( " - " )
-  Oo0oOOo = " - " . join ( i11I1iIiII [ 1 : ] )
-  cid = i11I1iIiII [ 0 ]
-  if 22 - 22: I11i11Ii / ii1I * I11i11Ii * Iii1IIIiiI . i1iII1I1i1i1 / ii1I
- i1IIi11111i ( "Browse YT Playlist by Channel [%s]" % Oo0oOOo , "/ytcp/%s/%s/%s" % ( Oo0oOOo , cid , page ) )
+  OOoo0O = cid . split ( " - " )
+  OOO0o = " - " . join ( OOoo0O [ 1 : ] )
+  cid = OOoo0O [ 0 ]
+  if 79 - 79: O0O / ooO0OO000o
+ i1IIi11111i ( "Browse YT Playlist by Channel [%s]" % OOO0o , "/ytcp/%s/%s/%s" % ( OOO0o , cid , page ) )
  oO00oOo = IiII ( "%s/ytcp/%s/%s" % ( oooo , cid , page ) )
  for iII11i in oO00oOo :
   iII11i [ "path" ] = iI11I1II1I1I + iII11i [ "path" ]
-  OoOO0oo0o ( iII11i )
+  O00O0oOO00O00 ( iII11i )
  if O0 . get_setting ( 'thumbview' , bool ) :
   if xbmc . getSkinDir ( ) in ( 'skin.confluence' , 'skin.eminence' ) :
    return O0 . finish ( oO00oOo , view_mode = 500 )
@@ -320,114 +320,114 @@ def Oo0ooOo0o ( cid , page = "" ) :
    return O0 . finish ( oO00oOo )
  else :
   return O0 . finish ( oO00oOo )
-  if 2 - 2: iI / ooO0OO000o / iiI1i1 % Ii11111i % I1Ii
+  if 75 - 75: Ii11111i % iiI1i1 % iiI1i1 . Ooooo
 @ O0 . route ( '/play/<url>' )
 @ O0 . route ( '/play/<url>/<title>' )
-def o0o00OO0 ( url , title = "" ) :
+def III1iII1I1ii ( url , title = "" ) :
  i1IIi11111i ( "Play [%s]" % title , "/play/%s/%s" % ( title , url ) )
- O0 . set_resolved_url ( i1I1ii ( url ) , subtitles = "https://raw.githubusercontent.com/vinhcomp/xml/master/xml/sub1.tsv" )
- if 61 - 61: Iii1IIIiiI
-def i1I1ii ( url ) :
+ O0 . set_resolved_url ( oOOo0 ( url ) , subtitles = "https://docs.google.com/spreadsheets/d/16l-nMNyOvrtu4FKLm-ctGDNClCjI09XKp3lcOKPOXMk/export?format=tsv&gid=0" )
+ if 54 - 54: ooO0OO000o - O0O0O0O00OooO % i1iII1I1i1i1
+def oOOo0 ( url ) :
  if "youtube" in url :
-  O0OOO = re . compile ( '(youtu\.be\/|youtube-nocookie\.com\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v|user)\/))([^\?&"\'>]+)' ) . findall ( url )
-  II11iIiIIIiI = O0OOO [ 0 ] [ len ( O0OOO [ 0 ] ) - 1 ] . replace ( 'v/' , '' )
-  return 'plugin://plugin.video.youtube/play/?video_id=%s' % II11iIiIIIiI
+  OOoO = re . compile ( '(youtu\.be\/|youtube-nocookie\.com\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v|user)\/))([^\?&"\'>]+)' ) . findall ( url )
+  iII = OOoO [ 0 ] [ len ( OOoO [ 0 ] ) - 1 ] . replace ( 'v/' , '' )
+  return 'plugin://plugin.video.youtube/play/?video_id=%s' % iII
  elif "://" not in url :
   if url . startswith ( "uno-" ) :
-   o0o = url . strip ( ) . replace ( "uno-" , "" )
-   o00OooOO000 = "NTg4N2RkZmZjMzEyYmYxMDk0ZGU0YmQ1" . decode ( "base64" )
-   OOoOoo = {
+   ii1ii11IIIiiI = url . strip ( ) . replace ( "uno-" , "" )
+   O00OOOoOoo0O = "NTg4N2RkZmZjMzEyYmYxMDk0ZGU0YmQ1" . decode ( "base64" )
+   O000OOo00oo = {
  "Content-Type" : "application/x-www-form-urlencoded" ,
  "User-Agent" : "Dalvik/2.1.0" ,
  "Accept-Encoding" : "gzip"
  }
-   oO0000OOo00 = {
- "serial_id" : o00OooOO000 ,
- "query" : o0o
+   oo0OOo = {
+ "serial_id" : O00OOOoOoo0O ,
+ "query" : ii1ii11IIIiiI
  }
-   iiIi1IIiIi = requests . post (
+   ooOOO00Ooo = requests . post (
  "http://stbapi.v247tv.com/api/stb_channel2" ,
- headers = OOoOoo ,
- data = oO0000OOo00
+ headers = O000OOo00oo ,
+ data = oo0OOo
  ) . json ( ) [ "data" ]
-   oOooOoO0Oo0O = requests . get ( "http://echipstore.com:8000/uno/" + urllib . quote_plus ( iiIi1IIiIi ) )
-   oOO00Oo = re . compile ( "(\{.+?\})" ) . findall ( oOooOoO0Oo0O . text . strip ( ) ) [ 0 ]
-   return json . loads ( oOO00Oo ) [ "url" ]
+   oOooOoO0Oo0O = requests . get ( "http://echipstore.com:8000/uno/" + urllib . quote_plus ( ooOOO00Ooo ) )
+   IiIIIi1iIi = re . compile ( "(\{.+?\})" ) . findall ( oOooOoO0Oo0O . text . strip ( ) ) [ 0 ]
+   return json . loads ( IiIIIi1iIi ) [ "url" ]
   else :
-   i1iIIIi1i = requests . get ( "aHR0cDovL3d3dy52aWV0dHYyNC5jb20vbWFpbi9nZXRDaGFubmVsc0FwcEJveC5waHA=" . decode ( "base64" ) ) . json ( )
-   for iI1iIIiiii in i1iIIIi1i [ "channels" ] :
-    if iI1iIIiiii [ "channel_url" ] == url :
-     i1iI11i1ii11 = "aHR0cDovL3d3dy52aWV0dHYyNC5jb20vbWFpbi9nZXRTdHJlYW1pbmdTZXJ2ZXIucGhw" . decode ( "base64" )
-     iiIi1IIiIi = { 'strname' : '%s-' % iI1iIIiiii [ "channel_id" ] }
-     return requests . post ( i1iI11i1ii11 , data = iiIi1IIiIi ) . text . strip ( )
+   ooOOoooooo = requests . get ( "aHR0cDovL3d3dy52aWV0dHYyNC5jb20vbWFpbi9nZXRDaGFubmVsc0FwcEJveC5waHA=" . decode ( "base64" ) ) . json ( )
+   for II1I in ooOOoooooo [ "channels" ] :
+    if II1I [ "channel_url" ] == url :
+     O0i1II1Iiii1I11 = "aHR0cDovL3d3dy52aWV0dHYyNC5jb20vbWFpbi9nZXRTdHJlYW1pbmdTZXJ2ZXIucGhw" . decode ( "base64" )
+     ooOOO00Ooo = { 'strname' : '%s-' % II1I [ "channel_id" ] }
+     return requests . post ( O0i1II1Iiii1I11 , data = ooOOO00Ooo ) . text . strip ( )
  else :
   return url
-  if 58 - 58: o0OIiiIII111iI % ii1I . IiIi1Iii1I1 / O0OOo
+  if 9 - 9: ooO00oOoo / Oo - iI / O0O / I11i11Ii - iiI1i1
 @ O0 . route ( '/execbuiltin/<url>' )
-def O0o ( url ) :
+def o00oooO0Oo ( url ) :
  xbmc . executebuiltin ( 'XBMC.RunPlugin(%s)' % urllib . unquote_plus ( url ) )
- if 59 - 59: iI + iI + iiI1i1 / I11i11Ii
+ if 78 - 78: I1Ii % Ooooo + ooO00oOoo
 @ O0 . route ( '/showimage/<url>' )
-def O000oo ( url ) :
+def OOooOoooOoOo ( url ) :
  oo000 = xbmc . translatePath ( xbmcaddon . Addon ( ) . getAddonInfo ( 'path' ) ) . decode ( "utf-8" )
- IIi1I11I1II = xbmc . translatePath ( os . path . join ( oo000 , "tmp" ) )
- if os . path . exists ( IIi1I11I1II ) :
-  shutil . rmtree ( IIi1I11I1II )
- os . makedirs ( IIi1I11I1II )
+ o0OOOO00O0Oo = xbmc . translatePath ( os . path . join ( oo000 , "tmp" ) )
+ if os . path . exists ( o0OOOO00O0Oo ) :
+  shutil . rmtree ( o0OOOO00O0Oo )
+ os . makedirs ( o0OOOO00O0Oo )
  if ".zip" in url :
-  OooOoooOo = xbmc . translatePath ( os . path . join ( IIi1I11I1II , "temp.zip" ) )
-  urllib . urlretrieve ( url , OooOoooOo )
-  i1I1ii1II1iII ( OooOoooOo , IIi1I11I1II )
-  xbmc . executebuiltin ( "SlideShow(%s,recursive)" % IIi1I11I1II )
-  if 46 - 46: IiIi1Iii1I1
-  if 8 - 8: O0OOo * Ii11111i - I1Ii - o0OIiiIII111iI * i1iII1I1i1i1 % iI
+  iioOooOOOoOo = xbmc . translatePath ( os . path . join ( o0OOOO00O0Oo , "temp.zip" ) )
+  urllib . urlretrieve ( url , iioOooOOOoOo )
+  i1I1ii1II1iII ( iioOooOOOoOo , o0OOOO00O0Oo )
+  xbmc . executebuiltin ( "SlideShow(%s,recursive)" % o0OOOO00O0Oo )
+  if 41 - 41: I1Ii - ooO0OO000o - ooO0OO000o
+  if 68 - 68: i1iII1I1i1i1 % Ooooo
  else :
-  iioOooOOOoOo = xbmcgui . WindowDialog ( )
-  i1Iii1i1I = xbmcgui . ControlImage ( 0 , 0 , 1280 , 720 , url )
-  iioOooOOOoOo . addControl ( i1Iii1i1I )
-  iioOooOOOoOo . doModal ( )
-  if 91 - 91: ooO00oOoo + iI . i1iII1I1i1i1 * ooO00oOoo + iI * Oo
+  ooO00OO0 = xbmcgui . WindowDialog ( )
+  i11111IIIII = xbmcgui . ControlImage ( 0 , 0 , 1280 , 720 , url )
+  ooO00OO0 . addControl ( i11111IIIII )
+  ooO00OO0 . doModal ( )
+  if 19 - 19: Ii11111i * oo000i1iIi11iIIi1
 def i1IIi11111i ( title = "Home" , page = "/" ) :
  try :
-  O000OOOOOo = "http://www.google-analytics.com/collect"
-  Iiii1i1 = open ( OO ) . read ( )
-  oo000o = {
+  ii111iI1iIi1 = "http://www.google-analytics.com/collect"
+  OOO = open ( oo0OOo0 ) . read ( )
+  I11IiI = {
  'v' : '1' ,
  'tid' : 'UA-52209804-5' ,
- 'cid' : Iiii1i1 ,
+ 'cid' : OOO ,
  't' : 'pageview' ,
  'dp' : "Kodi4VNLauncher%s" % page ,
  'dt' : "[Kodi4VNLauncher] - %s" % title
  }
-  requests . post ( O000OOOOOo , data = urllib . urlencode ( oo000o ) )
+  requests . post ( ii111iI1iIi1 , data = urllib . urlencode ( I11IiI ) )
  except :
   pass
-  if 44 - 44: oo000i1iIi11iIIi1 % Iii1IIIiiI + i1iIIII
-def OoOO0oo0o ( item ) :
- I1I1I = "/" if item [ "path" ] [ - 1 ] != "/" else ""
- OoOO000 = re . compile ( '/dir/\d+/*$' ) . findall ( item [ "path" ] )
- i1Ii11i1i = [ "/ytc/" , "/ytcp/" , "ytp" ]
- if len ( OoOO000 ) > 0 or 'launcher/play/' in item [ "path" ] :
-  item [ "path" ] += I1I1I + urllib . quote_plus ( item [ "label" ] . encode ( "utf8" ) )
- elif any ( word in item [ "path" ] for word in i1Ii11i1i ) :
+  if 53 - 53: IiIi1Iii1I1 % Iii1IIIiiI . O0O0O0O00OooO - I11i11Ii - O0O0O0O00OooO * Iii1IIIiiI
+def O00O0oOO00O00 ( item ) :
+ ooO0oOOooOo0 = "/" if item [ "path" ] [ - 1 ] != "/" else ""
+ i1I1ii11i1Iii = re . compile ( '/dir/\d+/*$' ) . findall ( item [ "path" ] )
+ I1IiiiiI = [ "/ytc/" , "/ytcp/" , "ytp" ]
+ if len ( i1I1ii11i1Iii ) > 0 or 'launcher/play/' in item [ "path" ] :
+  item [ "path" ] += ooO0oOOooOo0 + urllib . quote_plus ( item [ "label" ] . encode ( "utf8" ) )
+ elif any ( word in item [ "path" ] for word in I1IiiiiI ) :
   oo000 = urllib . unquote_plus ( item [ "path" ] )
   if " - " not in oo000 :
-   o0oOOoo = re . compile ( '/yt[c,p]p?/(.+?)/?$' ) . findall ( oo000 ) [ 0 ] . split ( "/" ) [ 0 ]
-   oOo00O0oo00o0 = "%s - %s" % ( o0oOOoo , item [ "label" ] )
-   iiOOooooO0Oo = urllib . quote_plus ( o0oOOoo )
-   OOiIiIIi1 = urllib . quote_plus ( oOo00O0oo00o0 . encode ( "utf8" ) )
-   item [ "path" ] = item [ "path" ] . replace ( iiOOooooO0Oo , OOiIiIIi1 )
-   if 7 - 7: i1iIIIiI1I - Oo - O0OOo + i1iIIIiI1I
-iI1I11iiI1i = xbmc . translatePath ( 'special://userdata' )
-if os . path . exists ( iI1I11iiI1i ) == False :
- os . mkdir ( iI1I11iiI1i )
-OO = os . path . join ( iI1I11iiI1i , 'cid' )
-iiiI11 = os . path . join ( iI1I11iiI1i , 'search.p' )
-if 78 - 78: O0OOo % ooO0OO000o % I1Ii
-if os . path . exists ( OO ) == False :
- with open ( OO , "w" ) as OOooO :
+   o0OIiII = re . compile ( '/yt[c,p]p?/(.+?)/?$' ) . findall ( oo000 ) [ 0 ] . split ( "/" ) [ 0 ]
+   ii1iII1II = "%s - %s" % ( o0OIiII , item [ "label" ] )
+   Iii1I1I11iiI1 = urllib . quote_plus ( o0OIiII )
+   I1I1i1I = urllib . quote_plus ( ii1iII1II . encode ( "utf8" ) )
+   item [ "path" ] = item [ "path" ] . replace ( Iii1I1I11iiI1 , I1I1i1I )
+   if 30 - 30: O0O
+I1Ii1iI1 = xbmc . translatePath ( 'special://userdata' )
+if os . path . exists ( I1Ii1iI1 ) == False :
+ os . mkdir ( I1Ii1iI1 )
+oo0OOo0 = os . path . join ( I1Ii1iI1 , 'cid' )
+iiiI11 = os . path . join ( I1Ii1iI1 , 'search.p' )
+if 87 - 87: Oo . O0O0O0O00OooO
+if os . path . exists ( oo0OOo0 ) == False :
+ with open ( oo0OOo0 , "w" ) as OOooO :
   OOooO . write ( str ( uuid . uuid1 ( ) ) )
-  if 46 - 46: O0O . ii1I
+  if 75 - 75: i1iIIIiI1I + Ii11111i + iiI1i1 * i1iIIII % O0OOo . IiIi1Iii1I1
 if __name__ == '__main__' :
  O0 . run ( )
 # dd678faae9ac167bc83abf78e5cb2f3f0688d3a3
