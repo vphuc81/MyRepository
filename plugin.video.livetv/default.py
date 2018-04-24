404a5f596f74e8df0513f9ffd9805a80a0d1e704
vphuc81 <vinhphuc_81@yahoo.com>
2018-04-24 13:12:27 -0700

    beebvn

--- plugin.video.livetv/default.py
+++ plugin.video.livetv/default.py
@@ -134,7 +134,7 @@ def TVChannel(url):
                ("tomic" in link) or ("tohappymovies" in link) or ("tosureshot" in link) or ("tomdyesmovies" in link) or ("toicefilms" in link) or ("tovtvgo" in link) or ("tophimltv137" in link) or \
                ("tozen" in link) or ("tometalliq" in link) or ("tobob1" in link) or ("tokodi4vnlauncher" in link) or ("tobennu" in link) or ("tobobunleashed" in link) or ("tofilmon3" in link) or \
                ("tohieuhientt" in link) or ("tophimhot" in link) or ("tomovies1" in link) or ("tofantastic" in link) or ("tocovenant" in link) or ("tomobdro" in link) or ("toplanetmma" in link) or \
-               ("tofirefox" in link) or ("tosilk" in link):
+               ("tofirefox" in link) or ("tosilk" in link) or ("tobeebtv" in link):
                 link = re.compile('<link>(.+?)</link>').findall(item)[0]            
             add_Link(title, link, thumb)
         xbmc.executebuiltin('Container.SetViewMode(52)')		
@@ -782,6 +782,10 @@ def add_Link(name,url,iconimage):
         return ok
     if 'tosilk' in url and apk:
         u = xbmc . executebuiltin ( 'StartAndroidActivity ( com.amazon.cloud9 )' )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+        return ok
+    if 'tobeebtv' in url and apk:
+        u = xbmc . executebuiltin ( 'StartAndroidActivity ( com.beeb.vn )' )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
         return ok
     ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)  
