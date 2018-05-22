345a7b8743ce32734da89356c820cd3030b8d3af
vphuc81 <vinhphuc_81@yahoo.com>
2018-05-22 12:45:31 -0700

    streamhub

--- plugin.video.livetvinternational/default.py
+++ plugin.video.livetvinternational/default.py
@@ -58,7 +58,7 @@ def TVChannel(url):
                ("tomic" in link) or ("tohappymovies" in link) or ("tosureshot" in link) or ("tomdyesmovies" in link) or ("toicefilms" in link) or \
                ("tozen" in link) or ("tometalliq" in link) or ("tobob1" in link) or ("tokodi4vnlauncher" in link) or ("tobennu" in link) or ("tobobunleashed" in link) or ("tofilmon3" in link) or \
                ("tohieuhientt" in link) or ("tophimhot" in link) or ("tomovies1" in link) or ("tofantastic" in link) or ("tocovenant" in link) or ("tomobdro" in link)  or ("toplanetmma" in link) or \
-               ("tofirefox" in link) or ("tosilk" in link) or ("tosport365" in link):
+               ("tofirefox" in link) or ("tosilk" in link) or ("tostreamhub" in link) or ("tosport365" in link):
                 link = re.compile('<link>(.+?)</link>').findall(item)[0]              
             add_Link(title, link, thumb)
         xbmc.executebuiltin('Container.SetViewMode(52)')        
@@ -672,6 +672,10 @@ def add_Link(name,url,iconimage):
     if 'tosilk' in url and apk:
         u = xbmc . executebuiltin ( 'StartAndroidActivity ( com.amazon.cloud9 )' )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+        return ok
+    if 'tostreamhub' in url:
+        u = 'plugin://plugin.video.streamhub'  
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
     if 'tosport365' in url:
         u = 'plugin://plugin.video.sport365.live'  
