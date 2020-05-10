4e3117320ab881930be1b794e5fa1239353a9039
vphuc81 <vinhphuc_81@yahoo.com>
2020-05-10 11:50:12 -0700

    indi

---
+++ plugin.program.indigo/README.md
@@ -0,0 +1,2 @@
+# plugin.program.indigo
+Indigo Tool Development for Unofficial Kodi Addons - Kodi is a registered trademark of the XBMC Foundation. We are not connected to or in any other way affiliated with Kodi - DMCA: legal@tvaddons.co
---
+++ repository.xvbmc/service.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+'''
+import base64
+import xbmc
+quitforcing=base64.b64decode('eolc3BlY2lhbDovL2hvbWUvYWRkb25zL3JlcG9zaXRvcnkueHZibWM='[3:]).decode('utf-8')
+yourcrap=xbmc.translatePath((quitforcing))
+xbmc.log(str(yourcrap),level=xbmc.LOGDEBUG)
+import os
+if os.path.exists(yourcrap):
+ import shutil
+ xbmc.log('DEL(%s)'%(yourcrap),xbmc.LOGWARNING)
+ shutil.rmtree(yourcrap,ignore_errors=True)
+#EOF
+'''
